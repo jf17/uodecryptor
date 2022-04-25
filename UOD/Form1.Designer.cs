57e9b6f2c5c3aea93cec90ca8e7bdc49a2c0a2ac
Alex Alexandrovich <javafirst17@gmail.com>
2022-04-25 11:56:50 +0300

    Update UI

--- UOD/Form1.Designer.cs
+++ UOD/Form1.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.LAB_StatusIS = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // LAB_StatusIS
             // 
             this.LAB_StatusIS.AutoSize = true;
-            this.LAB_StatusIS.Location = new System.Drawing.Point(50, 202);
+            this.LAB_StatusIS.Location = new System.Drawing.Point(26, 153);
             this.LAB_StatusIS.Name = "LAB_StatusIS";
             this.LAB_StatusIS.Size = new System.Drawing.Size(35, 13);
             this.LAB_StatusIS.TabIndex = 0;
@@ -44,17 +46,39 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(34, 48);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(393, 153);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(126, 53);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Decript";
+            this.button1.Text = "Decript it !";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(570, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "For your safety (seems to corrupt other files in the folder sometimes) copy only " +
+    "your \"client.exe\" into the decryptor folder. ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(307, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "The decryptor generates a new exe file \"Decrypted_client.exe\".";
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(598, 435);
+            this.ClientSize = new System.Drawing.Size(596, 241);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.LAB_StatusIS);
             this.Name = "Form1";
@@ -70,6 +94,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label LAB_StatusIS;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
